59aae2d7b08949fa1f35410ce99081a49f7539f1
allanpetergiffy <wallanpetergiffy@gmail.com>
2026-04-17 12:38:50 +0530

    Create factorial_of_n.c

---
+++ factorial_of_n.c
@@ -0,0 +1,16 @@
+// factorial of n
+
+#include <stdio.h>
+int main()
+{
+   int n,i,fact=1;
+   printf("n:");
+   scanf("%d",&n);
+  
+   for(i=1;i<=n;i++)
+   {
+      fact=fact*i; 
+   }
+    printf("factorial of %d = %d",n,fact);
+    return 0;
+}
